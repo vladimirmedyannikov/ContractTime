48e962061143d8876c270957eb2b87e19291bf5b
Vladimir <vladimir.medyannikov@gmail.com>
2016-01-11 00:24:00 +0400

    DAO implementation

--- src/ru/medyannikov/dao/factory/FirebirdDAOFactory.java
+++ src/ru/medyannikov/dao/factory/FirebirdDAOFactory.java
@@ -37,7 +37,8 @@ public class FirebirdDAOFactory extends DAOFactory {
     public Connection getConnection() throws DAOException {
         Connection connection = null;
         try {
-            String path = new File(DAOFactory.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsoluteFile().getParentFile().toString();
+            //String path = new File(DAOFactory.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsoluteFile().getParentFile().toString();
+            String path =new File("").getAbsoluteFile().getAbsolutePath();
             BufferedReader br = new BufferedReader(new FileReader(path + "\\conf.ini"));
             //new Alert(Alert.AlertType.NONE, path, ButtonType.OK).show();
             String url = br.readLine();
